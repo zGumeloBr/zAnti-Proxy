29339ab7e6df6e5946d02177f2c53ef1427d4945
zGumeloBr <66852780+zGumeloBr@users.noreply.github.com>
2021-03-26 15:19:23 -0300

    Update zProxyFilter.sh

--- zProxyFilter.sh
+++ zProxyFilter.sh
@@ -6,8 +6,7 @@
 #
 #
 
-#Verificações importantes
-
+#Verificando dependências.
 if [[ $EUID -ne 0 ]]; then
   echo -e "\033[1;31m * Esse script precisa ser executado em super usuário (sudo). \033[0m" 1>&2
   exit 1
@@ -15,130 +14,59 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 if ! [ -x "$(command -v wget)" ]; then
-  echo "\033[1;31m * O comando (wget) não foi encontrado, por favor instale-o\033[0m"
+  echo -e "\033[1;31m * O comando (wget) não foi encontrado, por favor instale-o\033[0m"
   exit 1
 fi
 
 if ! [ -x "$(command -v iptables)" ]; then
-  echo "\033[1;31m * O comando (iptables) não foi encontrado, por favor instale-o\033[0m"
+  echo -e "\033[1;31m * O comando (iptables) não foi encontrado, por favor instale-o\033[0m"
   exit 1
 fi
 
 if ! [ -x "$(command -v ipset)" ]; then
-  echo "\033[1;31m * O comando (ipset) não foi encontrado, por favor instale-o\033[0m"
+  echo -e "\033[1;31m * O comando (ipset) não foi encontrado, por favor instale-o\033[0m"
   exit 1
 fi
 
-# Criando tabela para bloquear as proxies.
-
+#Criando tabela para bloqueio.
 ipset create zProxyFilter -exist hash:ip hashsize 9999999 maxelem 9999999 timeout 0
 
-#
-#
-# Metódo (MPB)
-#
-#
+#Download das proxies.
+wget -O proxies.txt https://api.zgsite.tech/proxies.txt
+wget -O mproxies.txt https://api.zgsite.tech/mproxies.txt
 
-wget -O mpb.toml https://raw.githubusercontent.com/zGumeloBr/zMPB-Module/main/proxies.txt
+#Compactando em 1 só arquivo.
+cat mproxies.txt >> proxies.txt
 
-#
-#
-# Metódo (APB)
-#
-#
-
-wget -O pl1.toml https://www.proxy-list.download/api/v1/get?type=socks4
-
-wget -O pl2.toml https://api.proxyscrape.com/?request=displayproxies&proxytype=all&timeout=all&country=all
-
-wget -O pl3.toml https://raw.githubusercontent.com/ktsaou/blocklist-ipsets/master/nixspam.ipset
-
-wget -O pl4.toml https://check.torproject.org/torbulkexitlist
-
-wget -O pl5.toml https://www.proxy-list.download/api/v1/get?type=socks5
-
-wget -O pl6.toml https://www.proxyscan.io/download?type=socks4
-
-wget -O pl7.toml https://www.proxyscan.io/download?type=socks5
-
-wget -O pl8.toml https://api.proxyscrape.com/v2/?request=getproxies
-
-wget -O pl9.toml https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt
-
-wget -O pl10.toml https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/socks5.txt
-
-wget -O pl11.toml https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/socks4.txt
-
-wget -O pl12.toml https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/https.txt
-
-wget -O pl13.toml https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/http.txt
-
-wget -O pl14.toml https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt
-
-wget -O pl15.toml https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt
-
-wget -O pl16.toml https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt
-
-wget -O pl17.toml https://www.proxyscan.io/download?type=https
-
-wget -O pl18.toml https://www.proxyscan.io/download?type=http
-
-wget -O pl19.toml https://www.proxyscan.io/download?type=socks4
-
-wget -O pl20.toml https://www.proxyscan.io/download?type=socks5
-
-wget -O pl21.toml https://raw.githubusercontent.com/hookzof/socks5_list/master/proxy.txt
-
-wget -O pl22.toml https://www.proxy-list.download/api/v1/get?type=http
-
-wget -O pl23.toml https://www.proxy-list.download/api/v1/get?type=https
-
-wget -O pl24.toml http://www.proxylists.net/socks4.txt
-
-wget -O pl25.toml http://www.proxylists.net/http_highanon.txt
-
-wget -O pl26.toml https://multiproxy.org/txt_all/proxy.txt
-
-# Compactando todas as proxies em um arquivo.
-
-cat mpb.toml pl1.toml pl2.toml pl3.toml pl4.toml pl5.toml pl6.toml pl7.toml pl8.toml pl9.toml pl10.toml pl11.toml pl12.toml pl13.toml pl14.toml pl15.toml pl16.toml pl17.toml pl18.toml pl19.toml pl20.toml pl21.toml pl22.toml pl23.toml pl24.toml pl25.toml pl26.toml >> proxies.txt
-
-# Replace para retirar as portas e caracteres inuteis.
-
-sed -i 's/$/:/' proxies.txt
-
-sed -i 's/:.*/:/' proxies.txt
-
-sed -i 's/[:]\+//g' proxies.txt
-
-sed -i '/^#/d' proxies.txt
-
-sed -i '/^$/d' proxies.txt
-
-sed -i '/^This/d' proxies.txt
-
-sed -i 's/[a-z]\+//g' proxies.txt
-
-sed -i 's/[A-Z]\+//g' proxies.txt
-
-# Bloqueando todos as proxys coletadas.
-
+#Implementando as regras no firewall.
 for x in $(cat proxies.txt)
 do
         ipset -A zProxyFilter $x
 done
-
 iptables -I INPUT -m set --match-set zProxyFilter src -j DROP
 
-#Limpando arquivos de proxies antigos
-
-rm *.toml
-
+#Deletando arquivos após a instalação.
+rm mproxies.txt
 rm proxies.txt
 
-#Modulos adicionais e finalização.
+#Atualização automatica de proxies.
 
-clear
+timedatectl set-timezone America/Sao_Paulo
+
+cd /home
+
+touch crontab.txt
+
+echo "2 12 * * * cd /home && sh coletor.sh" >> crontab.txt
+
+crontab -i crontab.txt
+
+wget -O coletor.sh https://api.zgsite.tech/updater.txt
+
+chmod +x coletor.sh
+
+
+#Módulo azure instalação.
 echo -e "\033[1;34m* Você deseja ativar o módulo de bloqueio [Azure] ? \033[0m"
 echo -e "\033[1;34m* Ative este recurso somente se não estiver hospedando seu projeto na azure.  \033[0m"
 echo -e "\033[1;34m* Informações sobre: https://github.com/zGumeloBr/zPF-Azure-Module \033[0m"
@@ -147,30 +75,22 @@ read -r CONFIRM
 
 if [[ "$CONFIRM" =~ [Yy] ]]; then
     wget -O azure.txt https://raw.githubusercontent.com/zGumeloBr/zPF-Azure-Module/main/azure.txt
-    sed -i 's/$/:/' azure.txt
-    sed -i 's/:.*/:/' azure.txt
-    sed -i 's/[:]\+//g' azure.txt
-    sed -i '/^#/d' azure.txt
-    sed -i '/^$/d' azure.txt
-    sed -i 's/[a-z]\+//g' azure.txt
-    sed -i 's/[A-Z]\+//g' azure.txt
     for x in $(cat azure.txt)
     do
-        ipset -A zProxyFilter $x
+            ipset -A zProxyFilter $x
     done
-
     iptables -I INPUT -m set --match-set zProxyFilter src -j DROP
     rm azure.txt
     clear
     echo ""
     echo ""
     echo -e "\033[1;34m ____  ____  ____  _____  _  _  _  _  ____  ____  __    ____  ____  ____ \033[0m"  
-    echo -e "\033[1;34m(_   )(  _ \(  _ \(  _  )( \/ )( \/ )( ___)(_  _)(  )  (_  _)( ___)(  _ \\033[0m" 
+    echo -e "\033[1;34m(_   )(  _ \(  _ \(  _  )( \/ )( \/ )( ___)(_  _)(  )  (_  _)( ___)(  _ \ \033[0m" 
     echo -e "\033[1;34m / /_  )___/ )   / )(_)(  )  (  \  /  )__)  _)(_  )(__   )(   )__)  )   / \033[0m"
     echo -e "\033[1;34m(____)(__)  (_)\_)(_____)(_/\_) (__) (_)   (____)(____) (__) (____)(_)\_) \033[0m"
     echo ""
-    echo -e "\033[0;37m❖ Proxys bloqueadas com sucesso! Lembre-se de sempre re-ativar\033[0m"
-    echo -e "\033[0;37messa proteção para atualizar as lista de proxys bloqueadas. \033[0m"
+    echo -e "\033[0;37m❖ Proteção instalada com sucesso! Lembre-se sempre de acompanhar\033[0m"
+    echo -e "\033[0;37matualizações em nosso github, assim sempre se mantendo protegido.\033[0m"
     echo ""
     echo -e "\033[1;33m❖ Módulos adicionais bloqueados:\033[0m"
     echo ""
@@ -182,11 +102,11 @@ if [[ "$CONFIRM" =~ [Nn] ]]; then
     echo ""
     echo ""
     echo -e "\033[1;34m ____  ____  ____  _____  _  _  _  _  ____  ____  __    ____  ____  ____ \033[0m"  
-    echo -e "\033[1;34m(_   )(  _ \(  _ \(  _  )( \/ )( \/ )( ___)(_  _)(  )  (_  _)( ___)(  _ \\033[0m" 
+    echo -e "\033[1;34m(_   )(  _ \(  _ \(  _  )( \/ )( \/ )( ___)(_  _)(  )  (_  _)( ___)(  _ \ \033[0m" 
     echo -e "\033[1;34m / /_  )___/ )   / )(_)(  )  (  \  /  )__)  _)(_  )(__   )(   )__)  )   / \033[0m"
     echo -e "\033[1;34m(____)(__)  (_)\_)(_____)(_/\_) (__) (_)   (____)(____) (__) (____)(_)\_) \033[0m"
     echo ""
-    echo -e "\033[0;37m❖ Proxys bloqueadas com sucesso! Lembre-se de sempre re-ativar\033[0m"
-    echo -e "\033[0;37messa proteção para atualizar as lista de proxys bloqueadas. \033[0m"
+    echo -e "\033[0;37m❖ Proteção instalada com sucesso! Lembre-se sempre de acompanhar\033[0m"
+    echo -e "\033[0;37matualizações em nosso github, assim sempre se mantendo protegido.\033[0m"
     echo ""
 fi
